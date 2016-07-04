fd997052a0fca699938ceb0f2860ccb1270f17cf
Tasdik Rahman <tasdik.rahman@wingify.com>
2016-07-04 15:03:21 +0530

    docker: Testing it

--- bumblebee/constants.py
+++ bumblebee/constants.py
@@ -17,7 +17,7 @@ FILE_NAME = os.path.abspath(__file__)
 DIRNAME = os.path.dirname(FILE_NAME)
 SETTINGS_FILE = os.path.join(DIRNAME, 'config.ini')
 
-"""check whether the file 'settings.ini' exists or not"""
+"""check whether the file 'config.ini' exists or not"""
 if os.path.isfile(SETTINGS_FILE):
     import configparser
     config = configparser.ConfigParser()
@@ -33,6 +33,14 @@ if os.path.isfile(SETTINGS_FILE):
         raise TokensNotSet("Set the appropriate tokens in 'config.ini'")
 
 else:
-    raise SettingFileNotFound("The file 'config.ini' was not set!")
+     # If the config.ini file does not exist
+    response = "The file 'config.ini' was not set!"\
+               "\nLooking for set environment variables"
+    # raise SettingFileNotFound(response)
+    SLACK_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
+    BOT_ID = os.environ.get('BOT_ID')
+    VWO_API_TOKEN = os.environ.get('VWO_API_TOKEN')
+    ACCOUNT_ID = os.environ.get('ACCOUNT_ID')
+    HEADERS = {'content-type': 'application/json', 'token': VWO_API_TOKEN}
 
 AT_BOT = "<@" + BOT_ID + ">:"
